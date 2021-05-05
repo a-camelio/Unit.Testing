16902d4720c0c2a3424f5ecffe45b6ae3a79361a
Devin McDermott <dc.mcdermott22@gmail.com>
2021-05-05 16:19:15 -0400

    Transaction Tests
    Transaction tests implemented
    
    Devin McDermott <dc.mcdermott22@gmail.com>

--- src/main/java/TransactionTest.java
+++ src/main/java/TransactionTest.java
@@ -1,26 +1,38 @@
+import banking.Transaction;
+import banking.TransactionType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
+    final Transaction t = new Transaction(TransactionType.DEPOSIT, 00.00, "Paycheck");
+
     @Test
     void getId() {
+        Assertions.assertEquals(1, t.getId());
     }
 
     @Test
     void getTimestamp() {
+        Assertions.assertEquals(LocalDateTime.now(), t.getTimestamp());
     }
 
     @Test
     void getType() {
+        Assertions.assertEquals(TransactionType.DEPOSIT, t.getType());
     }
 
     @Test
     void getAmount() {
+        Assertions.assertEquals(15.25, t.getAmount());
     }
 
     @Test
     void getDescription() {
+        Assertions.assertEquals("Paycheck", t.getDescription());
     }
 }
